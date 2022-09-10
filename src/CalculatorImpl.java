e583341cf47ccd316800993be0c42f696df9dd9a
JaiVa-Coding <72058534+JaiVa-Coding@users.noreply.github.com>
2022-09-10 16:50:10 -0400

    Lambda Expressions Basic Syntax

---
+++ src/CalculatorImpl.java
@@ -0,0 +1,33 @@
+
+interface Calculator {
+//	void SwitchOn() ;
+	void Sum(int input);
+	}
+
+
+
+public class CalculatorImpl {
+	
+	public static void main(String[] args) {
+		//Example 1
+		/*
+		Calculator calculator = () -> {
+			System.out.println("Switched On");
+		};
+		
+		calculator.SwitchOn();
+		*/
+		//Example 2
+		/*
+		Calculator calculator = ( input) -> {
+			System.out.println("Sum : " + input);			
+		};
+		
+		calculator.Sum(5);
+		*/
+		
+		//Example 3
+		
+	}
+
+}
